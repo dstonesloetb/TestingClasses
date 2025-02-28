802404c81fc6a96ab6ca0bf9be8fc6b5d45d0ae9
dstonesloetb <145108402+dstonesloetb@users.noreply.github.com>
2025-02-28 11:04:54 +0000

    Create TestCustomer.java

---
+++ TestCustomer.java
@@ -0,0 +1,85 @@
+//Package Name
+
+//Import relevant classess if required
+
+//Class Declaration
+
+public class TestCustomer {
+
+   public static void main(String[] args) {
+
+     // Test all the constructors, setter and getter methods
+     // and toString method for the Customer class
+     // using the sample customers in the FOOP Brief
+
+     // Create ArrayList for Customer 1 
+
+     // Add Health conditions for Customer 1 to ArrayList
+
+     //Create Customer1 using full constructor
+
+     //Display Customer1 using toString() method
+
+
+     //*******************************************
+
+    // Create ArrayList for Customer 2 
+
+     // Add Health conditions for Customer 2 to ArrayList
+
+     //Create Customer2 using empty constructor
+
+     //Update Customer2 using setter methods
+
+     //Display Customer2 using getter methods
+
+     //*******************************************
+
+     // Create ArrayList for Customer 3
+
+     // Add Health conditions for Customer 3 to ArrayList
+
+     //Create Customer3 using full constructor
+
+     //Display Customer3 using toString() method
+
+
+     //*******************************************
+
+     // Create ArrayList for Customer 4
+
+     // Add Health conditions for Customer 4 to ArrayList
+
+     //Create Customer4 using full constructor
+
+     //Display Customer4 using toString() method
+
+
+    //*******************************************
+
+     // Create ArrayList for Customer 5
+
+     // Add Health conditions for Customer 5 to ArrayList
+
+     //Create Customer5 using full constructor
+
+     //Display Customer5 using toString() method
+     
+
+    //*******************************************
+
+     // Create ArrayList for Customer 6
+
+     // Add Health conditions for Customer 6 to ArrayList
+
+     //Create Customer6 using full constructor
+
+     //Display Customer6 using toString() method
+     
+   //*******************************************
+
+    //ctd as required. 
+     
+   }  //end main 
+
+} //end class 
