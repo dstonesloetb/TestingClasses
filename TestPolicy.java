aad24224046c4acde887052d5d543c99a05f6f06
dstonesloetb <145108402+dstonesloetb@users.noreply.github.com>
2025-02-28 13:16:06 +0000

    Update TestPolicy.java

--- TestPolicy.java
+++ TestPolicy.java
@@ -9,74 +9,35 @@ public class TestPolicy {
    public static void main(String[] args) {
 
      // Test all the constructors, setter and getter methods
-     // and toString method for the Customer class
-     // using the sample customers in the FOOP Brief
+     // and toString method for the Policy class
+     // using the sample policies in the FOOP Brief
 
-     // Create ArrayList for Customer 1 
+     // Create ArrayList for Policy 1 
 
-     // Add Health conditions for Customer 1 to ArrayList
+     //Create Policy 1 using full constructor
 
-     //Create Customer1 using full constructor
-
-     //Display Customer1 using toString() method
+     //Display Policy1 using toString() method
 
 
      //*******************************************
 
-    // Create ArrayList for Customer 2 
 
-     // Add Health conditions for Customer 2 to ArrayList
+     //Create Policy2 using empty constructor
 
-     //Create Customer2 using empty constructor
+     //Update Policy2 using setter methods
 
-     //Update Customer2 using setter methods
-
-     //Display Customer2 using getter methods
+     //Display Policy2 using getter methods
 
      //*******************************************
 
-     // Create ArrayList for Customer 3
 
-     // Add Health conditions for Customer 3 to ArrayList
+     //Create Policy3 using full constructor
 
-     //Create Customer3 using full constructor
-
-     //Display Customer3 using toString() method
+     //Display Policy3 using toString() method
 
 
      //*******************************************
 
-     // Create ArrayList for Customer 4
-
-     // Add Health conditions for Customer 4 to ArrayList
-
-     //Create Customer4 using full constructor
-
-     //Display Customer4 using toString() method
-
-
-    //*******************************************
-
-     // Create ArrayList for Customer 5
-
-     // Add Health conditions for Customer 5 to ArrayList
-
-     //Create Customer5 using full constructor
-
-     //Display Customer5 using toString() method
-     
-
-    //*******************************************
-
-     // Create ArrayList for Customer 6
-
-     // Add Health conditions for Customer 6 to ArrayList
-
-     //Create Customer6 using full constructor
-
-     //Display Customer6 using toString() method
-     
-   //*******************************************
 
     //ctd as required. 
      
