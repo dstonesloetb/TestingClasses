e925ff6b47a22e9a8a498ad51b0be0b9c13e5a5b
dstonesloetb <145108402+dstonesloetb@users.noreply.github.com>
2025-03-04 12:31:01 +0000

    Create TestCoroporatePolicy.java

---
+++ TestCoroporatePolicy.java
@@ -0,0 +1,32 @@
+//Package Name
+
+//Import relevant classess if required
+
+//Class Declaration
+
+public class TestCoroporatePolicy {
+
+   public static void main(String[] args) {
+
+     // Use the sample customers in the FOOP Brief
+
+     // Create ArrayList for Customer 1 Health Conditions
+
+     // Add Health conditions for Customer 1 to ArrayList
+
+     //Create Customer1 using full constructor
+
+     //Display Customer1 using toString() method
+
+     //Create CorporatePolicy 1
+
+     //Calculate premium for corporate policy 1
+     //e.g. cp1.calculateTotal(c1); 
+
+     //Display Corporate Policy 1
+
+     //Compare result with expected result in MS Excel
+
+     //*******************************************
+  
+     //Repeate process for additional customers and Individual Policies 
