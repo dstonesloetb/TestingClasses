97f93420a7a2e16e2a758ba33cd27b9ba1df2a6a
dstonesloetb <145108402+dstonesloetb@users.noreply.github.com>
2025-03-04 12:29:42 +0000

    Create TestIndividualPolicy.java

---
+++ TestIndividualPolicy.java
@@ -0,0 +1,35 @@
+//Package Name
+
+//Import relevant classess if required
+
+//Class Declaration
+
+public class TestIndividualPolicy {
+
+   public static void main(String[] args) {
+
+     // Use the sample customers in the FOOP Brief
+
+     // Create ArrayList for Customer 1 Health Conditions
+
+     // Add Health conditions for Customer 1 to ArrayList
+
+     //Create Customer1 using full constructor
+
+     //Display Customer1 using toString() method
+
+     //Create IndividualPolicy 1
+
+     //Calculate premium for individual policy 1
+     //e.g. p1.calculateTotal(c1); 
+
+     //Display Individual Policy 1
+
+     //Compare result with expected result in MS Excel
+
+     //*******************************************
+  
+     //Repeate process for additional customers and Individual Policies 
+
+
+  
